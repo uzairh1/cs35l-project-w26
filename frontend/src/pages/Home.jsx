04ee39dd93f4e2208f4bd369010786e83ad1589d
l-pamula <lpamula@g.ucla.edu>
2026-02-13 23:43:12 -0800

    Display health data on home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,35 @@
+import { useState, useEffect } from 'react';
+import { getHealth } from '/src/services/api.js';
+
+
+function HealthData() {
+  const [health, setHealth] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    fetch("/v1/projects/{ref}/health")
+    .then(response => response.json())
+    .then(jsonData => setHealth(jsonData));
+  }, []);
+
+
+  if (isLoading) return <div>Loading...</div>;
+
+
+  return (
+    <ul>
+      {health.items.map(item => (
+        <li key={item.id}>{item.name}</li>
+      ))}
+    </ul>
+  );
+}
+
+
 export default function Home() {
   return (
     <div>
       <h1>Home Page</h1>
+      <HealthData/>
     </div>
   );
 }
