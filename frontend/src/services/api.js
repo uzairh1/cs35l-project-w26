f04d618eae5cfcb3c8395de4e200df0d5b692093
Uzair Hammad <uzairhammad@hotmail.com>
2026-03-07 16:10:55 -0800

    fix minor typo

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -22,6 +22,7 @@ export async function loginUser(data) {
   const res = await fetch(`${API_URL}/login`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
   });
   return res.json();
 }
